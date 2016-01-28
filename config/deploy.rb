1cf747205b144d1e885c3729392274b4173886e0
Kevin Reiss <kevin.reiss@gmail.com>
2016-01-28 15:32:26 -0500

    Name app.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
+set :application, 'repecwp'
 set :repo_url, 'git@github.com:pulibrary/repecwp.git'
 
 # Default branch is :master
