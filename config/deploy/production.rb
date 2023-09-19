6cb8d91954f0a86f52c883b6a6233336f4de5b09
Francis Kayiwa <kayiwa@pobox.com>
2023-09-19 07:47:03 -0400

    add jammy vm

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -22,7 +22,8 @@ set :rails_env, 'production'
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-server 'repec-prod1', user: 'deploy', roles: %w{web app db}
+# server 'repec-prod1', user: 'deploy', roles: %w{web app db}
+server 'repec-prod2', user: 'deploy', roles: %w{web app db}
 
 
 
